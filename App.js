35fee274109bf7e03ce37df514dc286e91bdaf87
joielee <2j00923@gmail.com>
2021-07-10 23:33:51 +0900

    init repo

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Stack from './srcKr/modules/Stack'
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './Redux/Store'
 
-export default function App() {
+export default Basic = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Provider store={store}>
+    <NavigationContainer>
+      <Stack />
+    </NavigationContainer>
+    </Provider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Redux/Store.js
@@ -0,0 +1,126 @@
+import { createStore } from 'redux';
+
+const initState = {
+  tray: []
+}
+
+const personalState = {
+  userName:'joielee',
+  savedRecipe:[]
+}
+
+const initFlour = {
+  totalFlour: 0
+}
+
+
+const Reducer = ( state=initState, action ) => {
+  switch(action.type){
+    case 'deleteIgd':
+      console.log("deleted")
+      const item = action.value;
+      state.tray = state.tray.filter(cur=>cur.inputName!==item)
+      return {
+        ...state
+      }
+    case 'addIgd':
+      console.log("add")
+      state.tray.push(action.value)
+      // console.log("action value",state.tray);
+      return {
+        ...state
+      }
+    case 'brToCal':
+      state.tray = action.value.list;
+      // console.log("tray state: ",state.tray);
+      return {
+        ...state
+      }
+    case 'apply':
+      let totalFlour = parseInt(action.totalFlour);
+      let targetFlour = parseInt(action.targetFlour);
+      
+      console.log(
+        totalFlour,
+        targetFlour,
+      )
+      
+      if (targetFlour === '' || targetFlour === 0)
+        return{
+          ...state
+        }
+      
+      // infinity error in totalFlour===0
+      if (totalFlour === 0) {
+        state.tray.map(cur=>{
+          cur.percentage = 0.0;
+          cur.targetGram = 0.0;
+        })
+        return {
+          ...state
+        }
+      }
+
+      state.tray.map(cur=>{
+        cur.percentage = (((cur.inputGram / totalFlour).toFixed(3))*100).toFixed(1);
+        cur.targetGram = ((cur.inputGram / totalFlour)* parseInt(targetFlour)).toFixed(1);
+      })
+      return {
+        ...state
+      }
+    case 'reset':
+      return{
+        tray: []
+      }
+    case 'validity':
+      const result = state.tray.indexOf(action.value);
+      console.log("action value",action.value);
+      if (result === -1) return false;
+      else return true;
+    default:
+      return {
+        ...state
+      }
+  }
+}
+
+const personalReducer = (state = personalState, action) => {
+  switch(action.type){
+    case 'save':
+      return {
+        ...state
+      }
+    default:
+      return {
+        ...state
+      }
+  }
+}
+
+const FlourReducer = (state = initFlour, action) => {
+  switch (action.type) {
+    case 'addFlour':
+      state.totalFlour += parseInt(action.value.flour);
+      console.log('flour added', action.value, state.totalFlour);
+      return { ...state }
+    case 'removeFlour':
+      state.totalFlour -= parseInt(action.value.flour);
+      if (state.totalFlour < 0) state.totalFlour = 0;
+      console.log('flour removed', action.value, state.totalFlour);
+      return { ...state }
+    case 'passFlour':
+      state.totalFlour = parseInt(action.value.flour);
+      console.log('flour passing', action.value, state.totalFlour);
+      return { ...state }
+    case 'resetFlour':
+      state.totalFlour = 0;
+      console.log('flour passing', action.value, state.totalFlour);
+      return { ...state }
+    default:
+      return { ...state }
+  }
+}
+
+export const store = createStore(Reducer);
+export const personalStore = createStore(personalReducer);
+export const flourStore = createStore(FlourReducer);
